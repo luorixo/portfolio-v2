6e9f7cf5e0756860953e6c6c7390e20473a12246
luorixo <luorixo@gmail.com>
2022-08-18 19:12:15 +1200

    added custom scrollbar, bird render animations

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { motion } from "framer-motion";
+import { useState, useEffect } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import styled, { keyframes } from 'styled-components';
 
 import FeatherParticles from "../subComponents/FeatherParticles";
@@ -8,16 +8,16 @@ import MainContent from "./MainContent";
 
 // STYLED COMPONENTS
 const PageContainer = styled(motion.div)`
-
 `;
 
-const LandingContainer = styled(motion.div)`
-z-index: -1;
-background: #007859; 
+const LandingContainer = styled(motion.div )`
+z-index: 0;
+background: #007859;
 width: 100vw;
 height: 100vh;
 overflow: hidden;
 position: fixed;
+z-index: 0;
 `;
 
 const bounce = keyframes`
@@ -40,12 +40,11 @@ const bounce = keyframes`
     transform: translate(0, 0);
 }`
 
-const Center = styled.button` // the bird + text
+const Center = styled(motion.button)` // the bird + text
 z-index: 10;
 position: fixed;
-top: ${props => props.click ? '85%' :'50%'};
-left: ${props => props.click ? '92%' :'50%'};
-
+top: 85%;
+left: 92%;
 transform: translate(-50%, -50%);
 border: none;
 outline: none;
@@ -56,30 +55,45 @@ flex-direction: column;
 justify-content: center;
 align-items: center;
 transition: all 1.1s ease;
-z-index: 10;
 
 &>:first-child { // the bird
     animation: ${bounce} infinite 1.1s linear;
 }
 &>:last-child { // the 'click me' text
-    display: ${props => props.click ? 'none' : 'inline-block' };
     margin-top: 1rem;
 }`
 
 const Main = () => {
 
     const [click, setClick] = useState(false);
-    const handleClick = () => setClick(!click); // goes from true to false
+    const handleClick = () => {
+        setClick(!click); // goes from true to false
+        document.body.style.overflow = click ? "hidden" : "scroll"; // prevents scrolling on landing page
+    };
+
+    useEffect(() => {
+        document.body.style.overflow = "hidden";
+      }, []);
 
     return (
         <PageContainer>
-            <FeatherParticles/>
-            <LandingContainer/>
-            <Center>
-                <GreenBird style={{cursor:'pointer'}} onClick={()=> handleClick()} width={click ? 'min(14vw, 15vh)' : 'max(14vw, 20vh)'} 
-                height={click ? 'min(14vw, 15vh)' : 'max(14vw, 20vh)'} fill='currentColor' alt='clickable green bird icon'/><span style={{color:'#fff'}}>click me ↑</span>
+            <Center style={{top: click ? '87%' : '50%', left: click ? '92%' : '50%',}} initial={{ opacity: 0}} animate={{ opacity: 1}} transition={{duration: 0.05}}>
+                <GreenBird style={{cursor:'pointer', transition: '1.1s ease-out'}} onClick={()=> handleClick()} width={click ? 'min(14vw, 15vh)' : 'max(14vw, 20vh)'} 
+                height={click ? 'min(12vw, 13vh)' : 'max(14vw, 20vh)'} alt='clickable green bird icon'/><span style={{color:'#fff', display: click ? 'none' : 'block'}}>click me ↑</span>
             </Center>
-            {click ? <MainContent/> : <LandingContainer/>}
+            
+            {click ? null : <AnimatePresence><LandingContainer 
+            key="landingElement"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{duration: 1}}
+            exit={{ opacity: 1 }}
+
+            /></AnimatePresence>}
+            <MainContent/>
+            
+
+            <FeatherParticles/>
         </PageContainer>
     );
 }
--- client/src/components/MainContent.jsx
+++ client/src/components/MainContent.jsx
@@ -1,23 +1,43 @@
 import { useState } from "react";
-import { motion } from "framer-motion";
-import styled, { keyframes } from 'styled-components';
+import { motion, useScroll, useSpring } from "framer-motion";
+import styled from 'styled-components';
 
 // STYLED COMPONENTS 
 const MainContainer = styled(motion.div)`
 z-index: -1;
-background: #e9fffa; 
+background: rgb(255,255,255);
+background: linear-gradient(0deg, rgba(255,255,255,1) 0%, rgba(75,153,186,1) 100%); 
 width: 100vw;
-height: 200vh;
-overflow: hidden;
+height: 400vh;
 `;
 
+const ProgressBar = styled(motion.div)`
+position: fixed;
+bottom: 0;
+left: 0;
+right: 0;
+height: 1vh;
+width: 100vw;
+background: #007859;
+transform-origin: 0%;
+z-index: 0;
+border-radius: 0px;
+`;
 
 const Main = () => {
+    const { scrollYProgress } = useScroll();
+    const scaleX = useSpring(scrollYProgress, {
+        stiffness: 100,
+        damping: 20,
+        restDelta: 0.0001
+     });
+
     return (
         <MainContainer>
-
-            <div>yoyoyo</div>            
-            
+            <ProgressBar
+            style={{ scaleX }}
+            />
+            <div>yoyoyo</div>
         </MainContainer>
     );
 }
